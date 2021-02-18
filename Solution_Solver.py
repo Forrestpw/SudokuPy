48a7addda6a68620b53c865c32af77c17a394dc3
forrestpatwalker <forrestpatwalker@hotmail.com>
2021-02-18 12:15:40 -0700

    created recursive function that solves the sudoku puzzle using all the other functions.

--- Solution_Solver.py
+++ Solution_Solver.py
@@ -11,6 +11,26 @@ board = [
 ]
 
 
+def solve(bo):
+
+    find = find_empty(bo)
+    if not find:
+        return True
+    else:
+        row, col = find
+
+    for i in range(1, 10):
+        if valid(bo, i, (row, col)):
+            bo[row][col] = i
+
+            if solve(bo):
+                return True
+
+            bo[row][col] = 0
+
+    return False
+
+
 def valid(bo, num, pos):
 
     # Check row
@@ -56,3 +76,5 @@ def find_empty(bo):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
                 return (i, j) # row, col
+
+    return None
