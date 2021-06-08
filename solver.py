d86d3258a82db192e23276af92fdbbbe0df74160
Forrest Walker <forrestpatwalker@hotmail.com>
2021-06-08 12:29:03 -0600

    refactor: Remove redundant perentheses

--- solver.py
+++ solver.py
@@ -62,5 +62,5 @@ def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return (i, j) # row, col
+                return i, j  # row, col
     return None
